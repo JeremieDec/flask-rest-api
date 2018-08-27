fc8336d3b38fc0bf9a22f9f0b4907777acc495f7
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2018-08-27 13:46:37 -0500

    Is the image quality that doesn't help with the app

--- app/src/main/java/com/projects/enzoftware/apiqrreader/ui/MainActivity.kt
+++ app/src/main/java/com/projects/enzoftware/apiqrreader/ui/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.projects.enzoftware.apiqrreader.ui
 
 import android.Manifest
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import com.projects.enzoftware.apiqrreader.R
-import com.projects.enzoftware.apiqrreader.utils.getQrCodeInformation
+import com.projects.enzoftware.apiqrreader.utils.getQRCodeDetails
 import com.projects.enzoftware.apiqrreader.utils.requestPermission
 import io.fotoapparat.Fotoapparat
 import io.fotoapparat.log.fileLogger
@@ -14,9 +16,6 @@ import io.fotoapparat.log.loggers
 import io.fotoapparat.parameter.ScaleType
 import io.fotoapparat.selector.back
 import kotlinx.android.synthetic.main.activity_main.*
-import android.R.attr.bitmap
-import android.content.Intent
-import com.projects.enzoftware.apiqrreader.utils.getQRCodeDetails
 
 
 class MainActivity : AppCompatActivity() {
@@ -28,16 +27,14 @@ class MainActivity : AppCompatActivity() {
 
         val permissionInternetRequest = requestPermission(this, android.Manifest.permission.INTERNET)
         val permissionCameraRequest = requestPermission(this, Manifest.permission.CAMERA)
+        val permissionWESRequest = requestPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
         if(permissionCameraRequest){
             fotoapparat = Fotoapparat(
                     context = applicationContext,
                     view = camera_view,                   // view which will draw the camera preview
                     scaleType = ScaleType.CenterCrop,    // (optional) we want the preview to fill the view
                     lensPosition = back(),               // (optional) we want back camera
-                    logger = loggers(                    // (optional) we want to log camera events in 2 places at once
-                            logcat(),                   // ... in logcat
-                            fileLogger(applicationContext)            // ... and to file
-                    ),
                     cameraErrorCallback = { error ->
                         Log.e("CAMERA_ERROR",error.message)
                     }   // (optional) log fatal errors
@@ -52,9 +49,10 @@ class MainActivity : AppCompatActivity() {
             if(permissionInternetRequest){
                 photoResult
                         .toBitmap()
-                        .whenAvailable {bitmapPhoto ->
-                            //getQrCodeInformation(bitmapPhoto!!.bitmap)
-                            getQRCodeDetails(bitmapPhoto!!.bitmap)
+                        .whenAvailable {
+                            val bmp : Bitmap = BitmapFactory.decodeResource(applicationContext.resources,
+                                                                            R.drawable.frame)
+                            getQRCodeDetails(bmp)
                         }
             }
 
