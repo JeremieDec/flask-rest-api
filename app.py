c113ec3a56f788e827ca620ec868ccbe901aa4b5
enzoftware <lizama.enzo@gmail.com>
2019-01-02 20:00:52 -0500

    Migrations from flask

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Blueprint
 from flask_restful import Api
 from resources.Hello import Hello
 
-api_db = Blueprint('api', __name__)
-api = Api(api_db)
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
 
 # Route
 api.add_resource(Hello, '/hello')
--- resources/Hello.py
+++ resources/Hello.py
@@ -1,8 +1,8 @@
 from flask_restful import Resource
 
 
-class Hello:
-    def get():
+class Hello(Resource):
+    def get(self):
         return {"message": "Hello world!"}
 
     def post(self):
